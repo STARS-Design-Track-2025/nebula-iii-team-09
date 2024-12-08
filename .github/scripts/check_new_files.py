73b030c909793f9be90a168d963cdb3001d06ab7
AidanJacobsen <jacobse7@purdue.edu>
2024-12-08 11:24:44 -0500

    Update Approved Commit Hash

--- .github/scripts/check_new_files.py
+++ .github/scripts/check_new_files.py
@@ -9,7 +9,7 @@ TEAM_DV_PATTERN = re.compile(r"^verilog/dv/team_[0-9][0-9]/")
 TEAM_OPENLANE_PATTERN = re.compile(r"^openlane/team_[0-9][0-9]/")
 
 # Only change commit hash if you are a member of the post-program integration team.  All others will be banished off the face of the Earth
-STABLE_COMMIT_HASH = "fba9e021859b614b248f83a5723a445277073eac"  # 12/8/2024
+STABLE_COMMIT_HASH = "77cf694f4ebbca5c39f56ad2e3af0f8da0cc7d09"  # 12/8/2024
 
 # Get added files from git diff
 result = subprocess.run(
