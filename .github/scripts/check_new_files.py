6bf254dc9ffc69bfe7913a9349f9a7ac6a6841e2
Miguel Isrrael Teran <misrrael@purdue.edu>
2025-07-19 16:36:45 -0400

    Update commit hash (07/19/2025)

--- .github/scripts/check_new_files.py
+++ .github/scripts/check_new_files.py
@@ -9,7 +9,7 @@ INTEGRATION_MODE = False
 ALLOWED_DIRECTORIES = {".github", "def", "docs", "gds", "lef", "lib", "mag", "sdc", "signoff", "spef", "spi", "verilog/rtl/team_projects"}
 
 # Only change commit hash if you are a member of the post-program integration team.  All others will be banished off the face of the Earth
-STABLE_COMMIT_HASH = "b02eb6d1220af27c843e8f469c64fbbdc0e136ef"  # 07/18/2025
+STABLE_COMMIT_HASH = "8d4d8d734af9cc1986e912cf38d79ac91b6ba7f2"  # 07/19/2025
 
 
 # Patterns for per-team project areas
